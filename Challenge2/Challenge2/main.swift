a805ed7b2f79227c5efe0701902cae60e22d8597
Paulo Andrade <paulo@outercorner.com>
2019-06-15 20:36:17 +0100

    Added Challenge2

---
+++ Challenge2/Challenge2/main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Challenge2
+//
+//  Created by Paulo Andrade on 15/06/2019.
+//  Copyright © 2019 Paulo Andrade. All rights reserved.
+//
+
+import Foundation
+
+let base64Key           = "RlTBmTMO20iht3rMPrZxJoqx+2xYmFKC3zcchQus9QM="
+let base64Nonce         = "Gl2qCKBO7Bf6PhQKX8u63a8KAWwBWxYf"
+let base64CipherText    = """
+dpoMuV+aQm+X0DtNEPh0jnjpCVZRc1aJOfp9Ltk1qbblvgO4av5KTsugkM4Xu4cu
+Pt5b4NPuBQAXCDvMqNp792jEC/flzTYoRmSWnUC8nykcuYQn6as2x17lwDE0DTh5
+Bu+4g4HWROmuAQ0MZoGT8ClKxuLQJUvzHw3RrzbfZOyslNKEm03aVNM6v1ayxEXA
+3Fo73tSpTsD72KscTe2U+4m+hNc3ahRSJg==
+"""
+
+
+
