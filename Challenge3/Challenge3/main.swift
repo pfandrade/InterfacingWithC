327513687cd1362cfec91af33882c215c2c5a2c5
Paulo Andrade <paulo@outercorner.com>
2019-06-17 23:49:41 +0100

    Solved challenge 3

--- Challenge3/Challenge3/main.swift
+++ Challenge3/Challenge3/main.swift
@@ -22,10 +22,11 @@ struct Point: CustomStringConvertible {
 
 var points = [Point(x: 4, y: 10), Point(x: 50, y: -20), Point(x: 23, y: 11)]
 
-/*
- * use qsort to sort points by distance to origin
- */
-
+qsort(&points, points.count, MemoryLayout<Point>.stride) { (ptr1, ptr2) -> Int32 in
+    let p1 = ptr1!.load(as: Point.self)
+    let p2 = ptr2!.load(as: Point.self)
+    return Int32(p1.distanceToOrigin) - Int32(p2.distanceToOrigin)
+}
 
 print(points)
 
@@ -38,11 +39,30 @@ print(points)
  * hint: check qsort_r
  */
 
+extension Comparable {
+    static func compareRawPointers(l: UnsafeRawPointer, _ r: UnsafeRawPointer) -> Int32 {
+        let left: Self = l.load(as: Self.self)
+        let right: Self = r.load(as: Self.self)
+        if left < right { return -1 }
+        if left == right { return 0 }
+        return 1
+    }
+    
+}
+
+typealias CompareFunction = (UnsafeRawPointer, UnsafeRawPointer) -> Int32
+
+func globalCompare(thunk: UnsafeMutableRawPointer?, l: UnsafeRawPointer?, r: UnsafeRawPointer?) -> Int32 {
+    let compare = thunk!.load(as: CompareFunction.self)
+    return compare(l!,r!)
+}
 
 extension Array where Element: Comparable {
     func quicksorted() -> [Element] {
-        // TODO
-        return []
+        var array = self
+        var compare = Element.compareRawPointers
+        qsort_r(&array, array.count, MemoryLayout<Element>.stride, &compare, globalCompare)
+        return array
     }
     
     mutating func quicksort() {
