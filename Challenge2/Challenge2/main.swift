eda4d697f9429080a94b3604823abb5dc57a5356
Paulo Andrade <paulo@outercorner.com>
2019-06-20 13:49:03 +0100

    Added some guidance to Challenge2

--- Challenge2/Challenge2/main.swift
+++ Challenge2/Challenge2/main.swift
@@ -6,7 +6,18 @@
 //  Copyright © 2019 Paulo Andrade. All rights reserved.
 //
 
+/* For reference, the syntax of your module.modulemap file should look like this
+ module MyModule {
+    include "include/someheader.h"
+    export *
+ }
+ */
+
+
 import Foundation
+// 1 Start by exposing libsodium to Swift and then uncomment the following line replacing Module_Name with the name you chose on your module map
+
+// import Module_Name
 
 let base64Key           = "RlTBmTMO20iht3rMPrZxJoqx+2xYmFKC3zcchQus9QM="
 let base64Nonce         = "Gl2qCKBO7Bf6PhQKX8u63a8KAWwBWxYf"
@@ -22,3 +33,16 @@ let nonce = Data(base64Encoded: base64Nonce)!
 let cipherText = Data(base64Encoded: base64CipherText, options: .ignoreUnknownCharacters)!
 
 
+// 2 Now we need get some pointers to the memory of the key, nonce and cipherText.
+// Tip: You can either allocate a new memory chunk and copy the bytes there using Data.copyBytes or use the Data.withUnsafeBytes function.
+
+
+// 3 You also need some space to store the decrypted message
+//let messageLength = cipherText.count - Int(crypto_secretbox_MACBYTES)
+//let messagePtr = UnsafeMutablePointer<UInt8>.allocate(capacity: messageLength)
+//defer {
+//    messagePtr.deallocate()
+//}
+
+// 4 Finally you can call into C
+// the function you're looking for is crypto_secretbox_open_easy
